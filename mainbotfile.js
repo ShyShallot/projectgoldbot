d29695704ae2664c8d2d11fd1b11eb26d89d30ed
ShyShallot <commanderpandayt@gmail.com>
2022-07-03 20:56:56 -0700

    fixed commands

--- mainbotfile.js
+++ mainbotfile.js
@@ -79,7 +79,8 @@ bot.on('messageCreate', (message) =>{ // when someone sends a message
     //  bot.commands.get("name").execute(message, args, bot)
     //}
     cmd = bot.commands.get(command);
-    if(typeof cmd.admin === 'boolean' && cmd.admin){
+    if(typeof cmd === 'undefined'){return;}
+    if(cmd.admin){
         if(message.member.roles.cache.find(role => role.name === config.modrole)){
             cmd.execute(message,args,bot);
         }
