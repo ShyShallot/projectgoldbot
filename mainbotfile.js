e599e0242b6c310a53577298675aaf04eccf6e0c
ShyShallot <commanderpandayt@gmail.com>
2022-08-30 19:14:14 -0700

    i need sleep

--- mainbotfile.js
+++ mainbotfile.js
@@ -129,35 +129,32 @@ bot.on('messageCreate', async (message) =>{ // when someone sends a message
     //}
     cmd = bot.commands.get(command);
     console.log(`Command to Run: ${command}`);
-    if(typeof cmd === 'undefined'){
-        switch (command){
-            case 'cf':
-            case 'coinflip':
-                bot.commands.get("coinflip").execute(message,args,bot);
-                break;
-            case 'dep':
-            case 'deposit':
-                bot.commands.get("deposit").execute(message,args,bot);
-                break;
-            case 'bal':
-            case 'balance':
-                bot.commands.get("balance").execute(message,args,bot);
-                break;
-            case 'inv':
-            case 'inventory':
-                bot.commands.get("inventory").execute(message,args,bot);
-                break;
-            case 'lb':
-            case 'leaderboard':
-                bot.commands.get("leaderboard").execute(message,args,bot);
-                break;
-            case 'lvllb':
-                bot.commands.get("levellb").execute(message,args,bot);
-                break;
-            default:
-                return;
-        }
+    switch (command){
+        case 'cf':
+        case 'coinflip':
+            bot.commands.get("coinflip").execute(message,args,bot);
+            break;
+        case 'dep':
+        case 'deposit':
+            bot.commands.get("deposit").execute(message,args,bot);
+            break;
+        case 'bal':
+        case 'balance':
+            bot.commands.get("balance").execute(message,args,bot);
+            break;
+        case 'inv':
+        case 'inventory':
+            bot.commands.get("inventory").execute(message,args,bot);
+            break;
+        case 'lb':
+        case 'leaderboard':
+            bot.commands.get("leaderboard").execute(message,args,bot);
+            break;
+        case 'lvllb':
+            bot.commands.get("levellb").execute(message,args,bot);
+            break;
     }
+    if(typeof cmd === 'undefined'){return;}
     if(cmd.admin){
         if(message.member.roles.cache.find(role => role.name === guildConfig.modrole)){
             cmd.execute(message,args,bot,guildId);
